c349998f60bd8cbc3af27f5471d22b00da7b7243
Polux Troy <joffran712@gmail.com>
2024-07-11 05:02:31 +0200

    Fix EditNameForm

--- banking-app/src/components/EditNameForm.jsx
+++ banking-app/src/components/EditNameForm.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateUser, fetchUserProfile } from "../store/userSlice";
-
+import { updateUser } from "../store/userSlice";
 import "../components/EditNameForm.css";
 
 const EditNameForm = ({ onSave, onCancel }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
 
+  const [firstName] = useState(user?.firstName || "");
+  const [lastName] = useState(user?.lastName || "");
   const [userName, setUserName] = useState(user?.userName || "");
   const [updateSuccess, setUpdateSuccess] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const updatedUser = { userName };
+    const updatedUser = { firstName, lastName, userName };
     dispatch(updateUser(updatedUser)).then((response) => {
       if (response.meta.requestStatus === "fulfilled") {
         setUpdateSuccess(true);
-        dispatch(fetchUserProfile()); // Recharger les données de l'utilisateur
       }
     });
   };
@@ -37,6 +37,24 @@ const EditNameForm = ({ onSave, onCancel }) => {
           id="userName"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="firstName">First name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          disabled // Désactive le champ pour le rendre non modifiable
+        />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="lastName">Last name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          disabled // Désactive le champ pour le rendre non modifiable
         />
       </div>
       <button type="submit" className="edit-button">
